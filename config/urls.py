81a3363469b94c56591be0ae12087ff981a1e6b0
Christopher Bailey <cbailey@mort.is>
2020-07-27 10:14:38 -0400

    Linting

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views import defaults as default_views
-from django.views.static import serve
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 from boundlexx.api.urls import schema_view
 
