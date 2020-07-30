880f9bd5e0261f4b958407efbe2f6aa83c3d11f0
Christopher Bailey <cbailey@mort.is>
2020-07-30 12:22:28 -0400

    Removes squashed migrations

--- boundlexx/boundless/migrations/0009_testing_updates.py
+++ boundlexx/boundless/migrations/0009_testing_updates.py
@@ -4,120 +4,181 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
-    replaces = [('boundless', '0009_246_testing_updates'), ('boundless', '0010_name_fixing'), ('boundless', '0011_auto_20200728_1500'), ('boundless', '0012_auto_20200728_1328'), ('boundless', '0013_auto_20200730_1218')]
-
     dependencies = [
-        ('boundless', '0008_worldpoll_time'),
+        ("boundless", "0008_worldpoll_time"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='world',
-            name='region',
-            field=models.CharField(choices=[('use', 'US East'), ('usw', 'US West'), ('ecu', 'EU Central'), ('aus', 'Australia'), ('creative', 'Creative')], max_length=16, verbose_name='Server Region'),
+            model_name="world",
+            name="region",
+            field=models.CharField(
+                choices=[
+                    ("use", "US East"),
+                    ("usw", "US West"),
+                    ("ecu", "EU Central"),
+                    ("aus", "Australia"),
+                    ("creative", "Creative"),
+                ],
+                max_length=16,
+                verbose_name="Server Region",
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='region',
-            field=models.CharField(choices=[('use', 'US East'), ('usw', 'US West'), ('euc', 'EU Central'), ('aus', 'Australia'), ('creative', 'Creative')], max_length=16, verbose_name='Server Region'),
+            model_name="world",
+            name="region",
+            field=models.CharField(
+                choices=[
+                    ("use", "US East"),
+                    ("usw", "US West"),
+                    ("euc", "EU Central"),
+                    ("aus", "Australia"),
+                    ("creative", "Creative"),
+                ],
+                max_length=16,
+                verbose_name="Server Region",
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='description',
-            field=models.CharField(max_length=32, verbose_name='Description'),
+            model_name="world",
+            name="description",
+            field=models.CharField(max_length=32, verbose_name="Description"),
         ),
         migrations.AddField(
-            model_name='world',
-            name='assignment',
+            model_name="world",
+            name="assignment",
             field=models.PositiveSmallIntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='world',
-            name='owner',
+            model_name="world",
+            name="owner",
             field=models.PositiveSmallIntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='world',
-            name='number_of_regions',
+            model_name="world",
+            name="number_of_regions",
             field=models.PositiveSmallIntegerField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='address',
-            field=models.CharField(blank=True, max_length=128, null=True, verbose_name='Server Address'),
+            model_name="world",
+            name="address",
+            field=models.CharField(
+                blank=True,
+                max_length=128,
+                null=True,
+                verbose_name="Server Address",
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='api_url',
-            field=models.URLField(blank=True, null=True, verbose_name='API URL'),
+            model_name="world",
+            name="api_url",
+            field=models.URLField(
+                blank=True, null=True, verbose_name="API URL"
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='chunks_url',
-            field=models.URLField(blank=True, null=True, verbose_name='Chunks URL'),
+            model_name="world",
+            name="chunks_url",
+            field=models.URLField(
+                blank=True, null=True, verbose_name="Chunks URL"
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='ip_address',
-            field=models.GenericIPAddressField(blank=True, null=True, verbose_name='Server IP Address'),
+            model_name="world",
+            name="ip_address",
+            field=models.GenericIPAddressField(
+                blank=True, null=True, verbose_name="Server IP Address"
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='planets_url',
-            field=models.URLField(blank=True, null=True, verbose_name='Planets URL'),
+            model_name="world",
+            name="planets_url",
+            field=models.URLField(
+                blank=True, null=True, verbose_name="Planets URL"
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='websocket_url',
-            field=models.URLField(blank=True, null=True, verbose_name='Websocket URL'),
+            model_name="world",
+            name="websocket_url",
+            field=models.URLField(
+                blank=True, null=True, verbose_name="Websocket URL"
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='tier',
-            field=models.PositiveSmallIntegerField(choices=[(0, 'Placid (1)'), (1, 'Temperate (2)'), (2, 'Rugged (3)'), (3, 'Inhospitable (4)'), (4, 'Turbulent (5)'), (5, 'Fierce (6)'), (6, 'Savage (7)'), (7, 'Brutal (8)')], verbose_name='Tier'),
+            model_name="world",
+            name="tier",
+            field=models.PositiveSmallIntegerField(
+                choices=[
+                    (0, "Placid (1)"),
+                    (1, "Temperate (2)"),
+                    (2, "Rugged (3)"),
+                    (3, "Inhospitable (4)"),
+                    (4, "Turbulent (5)"),
+                    (5, "Fierce (6)"),
+                    (6, "Savage (7)"),
+                    (7, "Brutal (8)"),
+                ],
+                verbose_name="Tier",
+            ),
         ),
         migrations.AlterField(
-            model_name='world',
-            name='world_type',
-            field=models.CharField(choices=[('LUSH', 'Lush'), ('METAL', 'Metal'), ('COAL', 'Coal'), ('CORROSIVE', 'Corrosive'), ('SHOCK', 'Shock'), ('BLAST', 'Blast'), ('TOXIC', 'Toxic'), ('CHILL', 'Chill'), ('BURN', 'Burn'), ('UMBRIS', 'Umbris'), ('RIFT', 'Rift'), ('BLINK', 'Blink')], max_length=9, verbose_name='World Type'),
+            model_name="world",
+            name="world_type",
+            field=models.CharField(
+                choices=[
+                    ("LUSH", "Lush"),
+                    ("METAL", "Metal"),
+                    ("COAL", "Coal"),
+                    ("CORROSIVE", "Corrosive"),
+                    ("SHOCK", "Shock"),
+                    ("BLAST", "Blast"),
+                    ("TOXIC", "Toxic"),
+                    ("CHILL", "Chill"),
+                    ("BURN", "Burn"),
+                    ("UMBRIS", "Umbris"),
+                    ("RIFT", "Rift"),
+                    ("BLINK", "Blink"),
+                ],
+                max_length=9,
+                verbose_name="World Type",
+            ),
         ),
         migrations.AlterField(
-            model_name='resourcecount',
-            name='count',
-            field=models.PositiveIntegerField(verbose_name='Count'),
+            model_name="resourcecount",
+            name="count",
+            field=models.PositiveIntegerField(verbose_name="Count"),
         ),
         migrations.AlterField(
-            model_name='localizedname',
-            name='name',
-            field=models.CharField(db_index=True, max_length=128, verbose_name='Name'),
+            model_name="localizedname",
+            name="name",
+            field=models.CharField(
+                db_index=True, max_length=128, verbose_name="Name"
+            ),
         ),
         migrations.AddField(
-            model_name='world',
-            name='is_creative',
+            model_name="world",
+            name="is_creative",
             field=models.BooleanField(db_index=True, default=False),
         ),
         migrations.AddField(
-            model_name='world',
-            name='is_locked',
+            model_name="world",
+            name="is_locked",
             field=models.BooleanField(db_index=True, default=False),
         ),
         migrations.AddField(
-            model_name='world',
-            name='is_public',
+            model_name="world",
+            name="is_public",
             field=models.BooleanField(db_index=True, default=True),
         ),
         migrations.AlterField(
-            model_name='worldpoll',
-            name='time',
+            model_name="worldpoll",
+            name="time",
             field=models.DateTimeField(auto_now_add=True),
         ),
         migrations.AlterModelOptions(
-            name='leaderboardrecord',
-            options={'ordering': ['world_rank']},
+            name="leaderboardrecord", options={"ordering": ["world_rank"]},
         ),
         migrations.AlterModelOptions(
-            name='resourcecount',
-            options={'ordering': ['-count']},
+            name="resourcecount", options={"ordering": ["-count"]},
         ),
     ]
