82ab7f0ed26911c09b0e6da81c66ad19707543e8
Christopher Bailey <cbailey@mort.is>
2020-08-26 20:02:21 -0400

    Adds debug logging for Discord Webhooks

--- boundlexx/notifications/tasks.py
+++ boundlexx/notifications/tasks.py
@@ -10,6 +10,10 @@ from config.celery_app import app
 logger = get_task_logger(__name__)
 
 
+# Discord Webhook rate limit is 30 request/min
+DISCORD_DELAY = 2
+
+
 @app.task
 def send_discord_webhook(webhook_url, data_list):
     with cache.lock("discord_webhook:lock", expire=10 * len(data_list)):
@@ -20,8 +24,8 @@ def send_discord_webhook(webhook_url, data_list):
             now = time.monotonic()
 
             time_since = now - last_call
-            if time_since < 1:
-                time.sleep(1 - time_since)
+            if time_since < DISCORD_DELAY:
+                time.sleep(DISCORD_DELAY - time_since)
 
             response = requests.post(
                 webhook_url,
@@ -29,6 +33,8 @@ def send_discord_webhook(webhook_url, data_list):
                 headers={"Content-Type": "application/json"},
             )
             response.raise_for_status()
+            logger.debug(response.headers)
+            logger.debug(response.text)
 
             last_call = time.monotonic()
             cache.set(cache_key, last_call, timeout=1)
