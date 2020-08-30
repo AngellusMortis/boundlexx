838304359cbd200a3d50727554317396e776a2da
Christopher Bailey <cbailey@mort.is>
2020-08-30 16:53:45 -0400

    Fixes reset price lock

--- boundlexx/boundless/management/commands/reset_price_lock.py
+++ boundlexx/boundless/management/commands/reset_price_lock.py
@@ -6,4 +6,6 @@ from boundlexx.boundless.tasks.shop import UPDATE_PRICES_LOCK
 
 @click.command()
 def command():
-    cache.delete(UPDATE_PRICES_LOCK)
+    lock = cache.lock(UPDATE_PRICES_LOCK)
+
+    lock.reset()
