ad90b381413f7cddee50ca2ab90f921d747e3654
Christopher Bailey <cbailey@mort.is>
2020-09-08 00:30:14 -0400

    Caching test

--- boundlexx/api/views/item.py
+++ boundlexx/api/views/item.py
@@ -9,6 +9,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework_extensions.etag.decorators import etag
 from rest_framework_extensions.mixins import NestedViewSetMixin
 from rest_fuzzysearch.search import RankedFuzzySearchFilter
 
@@ -163,6 +164,7 @@ class ItemViewSet(
     ordering = ["-rank", "game_id"]
     ordering_fields: List[str] = []
 
+    @etag()
     @method_decorator(cache_page(settings.CACHE_DURATION))
     def list(self, request, *args, **kwargs):  # noqa A003
         """
