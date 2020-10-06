998a2655d767d05611448f7a3fb8e8e932b727f2
Christopher Bailey <cbailey@mort.is>
2020-10-06 14:54:54 -0400

    linting

--- boundlexx/api/views/util.py
+++ boundlexx/api/views/util.py
@@ -38,7 +38,7 @@ class ForumFormatAPIView(APIView):
         post = self.serializer_class(data=request.data)
 
         if post.is_valid() and post.world is not None:
-            if post.world.is_sovereign:
+            if post.world.is_sovereign:  # type: ignore
                 extra = {
                     "perms": {
                         "can_visit": post.data["can_visit"],
