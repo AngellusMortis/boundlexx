a78a76992427039f580239d0cd90118185e7ea7d
Christopher Bailey <cbailey@mort.is>
2021-04-14 15:56:46 -0400

    Adds redis cache purge to purge_cache

--- boundlexx/api/management/commands/purge_cache.py
+++ boundlexx/api/management/commands/purge_cache.py
@@ -1,10 +1,15 @@
 import djclick as click
+from django.core.cache import cache
 
 from boundlexx.api.tasks import purge_cache, purge_static_cache
+from boundlexx.boundless.utils import ITEM_COLOR_IDS_KEYS, WORLD_ITEM_COLOR_IDS_KEYS
 
 
 @click.command()
 def command():
+    click.echo("Purging redis caches...")
+    cache.delete(ITEM_COLOR_IDS_KEYS)
+    cache.delete(WORLD_ITEM_COLOR_IDS_KEYS)
     click.echo("Purging static files...")
     purge_static_cache()
     click.echo("Purging endpoints...")
