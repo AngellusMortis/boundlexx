627c1fd4857aa67126258b9dcf5a927460fc1100
Christopher Bailey <cbailey@mort.is>
2021-04-30 13:23:55 -0400

    Linting fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -356,10 +356,6 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-# ADMIN_TOOLS_MENU = "boundlexx.admin.menu.BoundlexxMenu"
-# ADMIN_TOOLS_INDEX_DASHBOARD = "boundlexx.admin.dashboard.BoundlexxIndexDashboard"
-# ADMIN_TOOLS_APP_INDEX_DASHBOARD = "boundlexx.admin.dashboard.BoundlexxAppIndexDashboard"
-
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
